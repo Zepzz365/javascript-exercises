db82e36dfe60597048e77c70aeb1c461fdb7a4ad
Zepzz365 <zeynep170107@gmail.com>
2025-08-05 20:04:30 +0300

    object basics

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        if (a.yearOfDeath === undefined){
+            a.yearOfDeath = 2025
+        }
+        if (b.yearOfDeath === undefined){
+            b.yearOfDeath = 2025
+        }
+        return b.yearOfDeath - b.yearOfBirth - a.yearOfDeath + a.yearOfBirth
+    })
+    return people[0]
 };
 
 // Do not edit below this line
